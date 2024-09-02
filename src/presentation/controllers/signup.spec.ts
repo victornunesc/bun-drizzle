00ecee435a3167648de05f1b98950372e65a4301
Victor Nunes <victornunesc95@gmail.com>
2024-09-02 14:09:52 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,18 @@
+import { describe, expect, test } from "bun:test";
+import { SignUpController } from "./signup";
+
+describe("SignUp Controoler", () => {
+	test("Should return 400 if no name is provided", () => {
+		const sut = new SignUpController();
+		const httpRequest = {
+			body: {
+				email: "john_doe@mail.com",
+				password: "my_password",
+				passwordConfirmation: "my_password",
+			},
+		};
+		const httpResponse = sut.handle(httpRequest);
+
+		expect(httpResponse.statusCode).toBe(400);
+	});
+});
