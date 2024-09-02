207170d171233babaf2dfd1a4971bdb6d4296390
Victor Nunes <victornunesc95@gmail.com>
2024-09-02 15:47:02 -0300

    feat: handle server errors in SignUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,6 @@
-import { InvalidParamError } from "../erros/invalid-param-error copy";
+import { InvalidParamError } from "../erros/invalid-param-error";
 import { MissingParamError } from "../erros/missing-param-error";
-import { badRequest } from "../helpers/http-helper";
+import { badRequest, serverError } from "../helpers/http-helper";
 import type { IController } from "../protocols/controller";
 import type { IEmailValidator } from "../protocols/email-validator";
 import type { IHttpRequest, IHttpResponse } from "../protocols/http";
@@ -13,25 +13,32 @@ export class SignUpController implements IController {
 	}
 
 	public handle(httpRequest: IHttpRequest): IHttpResponse {
-		const requiredFields = [
-			"name",
-			"email",
-			"password",
-			"passwordConfirmation",
-		];
-		for (const field of requiredFields) {
-			if (!httpRequest.body[field]) {
-				return badRequest(new MissingParamError(field));
+		try {
+			const requiredFields = [
+				"name",
+				"email",
+				"password",
+				"passwordConfirmation",
+			];
+			for (const field of requiredFields) {
+				if (!httpRequest.body[field]) {
+					return badRequest(new MissingParamError(field));
+				}
 			}
-		}
 
-		if (!this.emailValidator.isValid(httpRequest.body.email)) {
-			return badRequest(new InvalidParamError("email"));
-		}
+			const isValid = this.emailValidator.isValid(httpRequest.body.email);
 
-		return {
-			statusCode: 200,
-			body: {},
-		};
+			if (!isValid) {
+				return badRequest(new InvalidParamError("email"));
+			}
+
+			return {
+				statusCode: 200,
+				body: {},
+			};
+		} catch (error) {
+			// console.error(error);
+			return serverError();
+		}
 	}
 }
